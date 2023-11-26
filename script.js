380b09668e425ca427d566433e50a765215d90a6
Judith Morgan <112726749+judithmorgan@users.noreply.github.com>
2023-11-26 13:06:42 -0500

    created variables for the buttons and split screen into two

---
+++ script.js
@@ -0,0 +1,18 @@
+/* Functions i need 
+
+- to clear the button
+-delete item
+- show zero on screen when user enters 
+-the operators to work
+- the. to work 
+- the numbers to enter */
+
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operatorButtons = document.querySelectorAll('[data-operator]')
+const equalsButton = document.getElementById('equalsBtn')
+const clearButton = document.getElementById('clearBtn')
+const deleteButton = document.getElementById('deleteBtn')
+const pointButton = document.getElementById('pointBtn')
+const currentDisplayScreen = document.getElementById('currentDisplayScreen')
+const previousDisplayScreen = document.getElementById('previousDisplayScreen')
